68d8e886b9f351051121d4df2a7fc368b36b8997
Jeris Manning <the.goose.moose@gmail.com>
2019-11-05 18:40:08 -0700

    mvp commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-// const hubsRouter = require('./hubs/hubs-router.js');
-
+const blogPostsRouter = require('./blogPosts/blog-posts-router.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', blogPostsRouter);
 
 module.exports = server;
